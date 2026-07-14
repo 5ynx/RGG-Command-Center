a1c6d1a428c0a1c29c0a2e0ebe9cd55b78b7748b
AgustinusWidyanto <widyagustin43@gmail.com>
2026-07-14 10:20:16 +0700

    add condition for intercom three dot button

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8069',
+  // apiUrl: 'https://ifs360-sg.com/',
   apiUrl: 'http://localhost:8069',
   vmsUrl: 'http://localhost:8100/login-vms'
 };
